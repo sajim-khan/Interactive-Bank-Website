f18fcb0f62b83700f6aa9106f995fd227e1ff9db
sajim-khan <shossain1427@gmail.com>
2023-02-15 01:56:48 +0600

    added addition calculation

--- js/deposit.js
+++ js/deposit.js
@@ -3,12 +3,30 @@ document.getElementById('btn-deposit').addEventListener('click', function() {
     //step 2: get the deposit amount from the input field
     //For input field use .value to get the value inside the input field
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
     //console.log(depositAmount);
+    const newDepositAmount = parseFloat(newDepositAmountString);
     
     //step 3: get the current deposit amount  
     // for non-input (element other than input, textarea) use innerText to get the text
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotalAmount = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    
+    //step 4: add number to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount
+    // set the deposit total
+    depositTotalElement.innerText = currentDepositTotal;
+    
+    // step 5: get balance current total
+    const balanceTotalElement = document.getElementById('balance-total');
+    const  previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    
+    // step 6: calculate current total balance
+    const currentBalanceTotal = previousBalanceTotal + newBalanceAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+    
+    
 });
+
