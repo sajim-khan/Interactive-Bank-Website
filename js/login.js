4f005251184af58fddb5e05a5c79400c7016db89
sajim-khan <shossain1427@gmail.com>
2023-02-14 17:42:42 +0600

    added event listener and compare email & password

---
+++ js/login.js
@@ -0,0 +1,26 @@
+// Step : add the click event handler with the submit button 
+document.getElementById("btn-submit").addEventListener("click", function() {
+    // step 2: get the email address inside the email input field
+    // always remember to use .value to get text from input field
+    const emailField = document.getElementById("user-email");
+    const email = emailField.value;
+    //console.log(email);
+    
+    //step 3: get password
+    // 3.a : set id on the html element
+    // 3.b : get the element
+    // 3.c : get the value from the element
+    const passwordField = document.getElementById("user-password");
+    const password = passwordField.value;
+    //console.log(password);
+    
+    // step 4: check valid user
+    if(email === 'sajimkhan@gmail.com' && password === 'sajimkhan'){
+        console.log('valid user');
+    } else {
+        console.log('invalid user');
+    }
+});
+
+
+
